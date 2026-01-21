6128d61736d3eb9b703943c5dad8f4e38adcc612
derpymoo <zhengyijie183@gmail.com>
2026-01-22 00:35:13 +0800

    Level-1: Echo user input and exit on bye

--- src/main/java/Shinchan.java
+++ src/main/java/Shinchan.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Shinchan {
     private static final String LINE = "----------------------------------------";
 
     public static void main(String[] args) {
         Shinchan shinchan = new Shinchan();
-        shinchan.greet();
-        shinchan.exit();
+        shinchan.run();
+    }
+
+    public void run() {
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                exit();
+                break;
+            } else {
+                echo(input);
+            }
+        }
+        scanner.close();
+    }
+
+    public void echo(String input) {
+        System.out.println(LINE);
+        System.out.println(input);
+        System.err.println(LINE);
     }
 
     public void greet() {
