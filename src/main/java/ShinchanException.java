d18afe914fc81f036a493c5b4eb6cd2ebc7172e4
derpymoo <zhengyijie183@gmail.com>
2026-01-29 20:38:04 +0800

    Fix checkstyle for ShinchanException.java

--- src/main/java/ShinchanException.java
+++ src/main/java/ShinchanException.java
@@ -1,8 +1,14 @@
-
+/**
+ * Represents an exception specific to the Shinchan application.
+ */
 public class ShinchanException extends Exception {
 
+    /**
+     * Creates a ShinchanException with the specified error message.
+     *
+     * @param message Error message describing the exception
+     */
     public ShinchanException(String message) {
         super(message);
     }
-
 }
