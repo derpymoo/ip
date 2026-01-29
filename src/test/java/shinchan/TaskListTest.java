1d224caa0ba2251a47b3e093f14368728b8bc552
derpymoo <zhengyijie183@gmail.com>
2026-01-30 01:28:16 +0800

    A-JUnit: Add unit tests

---
+++ src/test/java/shinchan/TaskListTest.java
@@ -0,0 +1,40 @@
+package shinchan;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import shinchan.task.Task;
+import shinchan.task.Todos;
+
+/**
+ * Tests for {@link TaskList}.
+ */
+public class TaskListTest {
+
+    @Test
+    public void addTask_sizeIncreases() {
+        TaskList list = new TaskList();
+        Task task = new Todos("read book");
+
+        list.add(task);
+
+        assertEquals(1, list.size());
+        assertEquals(task, list.get(0));
+    }
+
+    @Test
+    public void removeTask_returnsRemovedTaskAndSizeDecreases() {
+        TaskList list = new TaskList();
+        Task task1 = new Todos("a");
+        Task task2 = new Todos("b");
+        list.add(task1);
+        list.add(task2);
+
+        Task removed = list.remove(0);
+
+        assertEquals(task1, removed);
+        assertEquals(1, list.size());
+        assertEquals(task2, list.get(0));
+    }
+}
